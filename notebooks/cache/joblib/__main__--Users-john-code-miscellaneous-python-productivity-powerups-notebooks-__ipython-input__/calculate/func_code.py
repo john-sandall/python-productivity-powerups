20fd9ccc3e3a46269e58f80d317726b0892ade81
John Sandall <2884159+john-sandall@users.noreply.github.com>
2020-05-05 17:52:28 +0100

    🤞 Init

---
+++ notebooks/cache/joblib/__main__--Users-john-code-miscellaneous-python-productivity-powerups-notebooks-__ipython-input__/calculate/func_code.py
@@ -0,0 +1,5 @@
+# first line: 1
+@memory.cache
+def calculate(x):
+    time.sleep(3)
+    return x * 10
---
+++ notebooks/logaru_example.py
@@ -0,0 +1,5 @@
+from loguru import logger
+
+logger.debug("That's it, beautiful and simple logging!")
+logger.info("This message is purely informational. ¯\_(ツ)_/¯")
+logger.error("😱😱😱😱😱")
